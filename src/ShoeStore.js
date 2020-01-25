824936084e39eb8535cf2531054f3517fb115d2e
Lakshmi Subbramanian <lakshmisubbramanian@Lakshmis-MBP-2.fios-router.home>
2020-01-25 15:51:04 -0500

    include stockx dependencies to load  sneaker meta

--- src/ShoeStore.js
+++ src/ShoeStore.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
-export const ShoeStore = ({ shoes }) => (<table>
-  {shoes.map(shoe => <tr><h4>{shoe.name}</h4><h6>{shoe.brand}</h6></tr>)}
-</table>);
+export const ShoeStore = ({ shoes }) => (
+<table>
+  {shoes.map(shoe => 
+  <tr><h4>{shoe.name}</h4>
+  <h6>{shoe.brand}</h6></tr>
+  )}
+</table>
+);
